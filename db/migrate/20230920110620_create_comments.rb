9a89b3a53d256b99283c34eacb7c95bf8d1d7d3a
Kgomotso (Troos) Nacane <kgomotsonacane@gmail.com>
2023-09-20 16:38:28 +0200

    Update db/migrate/20230920110620_create_comments.rb
    Co-authored-by: Andres Agudelo Guzman <anagudelogu@gmail.com>

--- db/migrate/20230920110620_create_comments.rb
+++ db/migrate/20230920110620_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :AuthorId
-      t.integer :PostId
+      t.references :author, foreign_key: { to_table: 'users' }
+      t.references :post, foreign_key: true
       t.text :Text
 
       t.timestamps
