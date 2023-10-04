5612bf96f13cabd7c24e571b1f374f243becbbb3
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-10-04 17:51:21 +0200

    Configure Devise with Confirmable and update user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home]
   before_action :set_user, only: [:show]
+
+  def home; end
+
   def index
     @users = User.all
   end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,16 @@
-# frozen_string_literal: true
-
 # Load the Rails application.
 require_relative 'application'
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "",
+   :password => ""
+ }
 
 # Initialize the Rails application.
 Rails.application.initialize!
